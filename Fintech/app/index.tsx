c008e15d2c6d264a03818fa44180de3e67f2a505
Hieu Cao <112494038+CodingHusk3y@users.noreply.github.com>
2024-11-19 22:02:03 -0500

    Set up Front end
    set up

---
+++ Fintech/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Trẻ con sa mạc truyền tai nhau bài đồng dao
+      </Text>
+    </View>
+  );
+}
