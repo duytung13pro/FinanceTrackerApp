7cd97429a47951ad266f02ef0b1eac475e85f99f
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2024-11-28 02:36:59 -0500

    Make simple navbar

--- Fintech/app/Header.tsx
+++ Fintech/app/Header.tsx
@@ -1,7 +1,57 @@
-import { Stack } from "expo-router";
-import LandingPage from "./LandingPage";
+import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { Colors } from "@/constants/Colors";
 
-<Stack>
-  <Stack.Screen name="LandingPage" options={{ title: 'Landing Page' }} />
-</Stack>;
+export default function Header() {
+    const routes = ["Register", "Login", "Dashboard", "About", "Profile"];
 
+    return (
+        <View style={style.navbar}>
+            {/* Logo links */}
+            <Link href="/" style={style.logoLink}>
+                <Image
+                    source={require("@/assets/images/cloud-logo.svg")}
+                    style={style.headerLogo}
+                />
+            </Link>
+            {/* Navigation links */}
+            <div style={style.navigationLinks}>
+                {routes.map((route) => (
+                    <Link href={"/" + route as any} style={style.navigationLink}>{route}</Link>
+                ))}
+            </div>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    navbar: {
+        padding: 20,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: Colors.dark.background,
+    },
+    logoLink: {
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    headerLogo: {
+        height: 50,
+        width: 70,
+    },
+    navigationLinks: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        gap: 20,
+    },
+    navigationLink: {
+        color: Colors.dark.text,
+        fontWeight: "600",
+    }
+});
